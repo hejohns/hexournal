b147cd610d0a913bc993ce93bc8351694e1eb20f
Johnson He <hejohns@umich.edu>
2021-05-01 22:53:35 +0000

    rework initUI
    (crashes on resize call cause undefined placeholders)

--- src/HeXournal/UI.hs
+++ src/HeXournal/UI.hs
@@ -14,34 +14,30 @@ import GI.Cairo.Render.Connector (renderWithContext)
 --import qualified GI.Gdk as Gdk
 import GHC.Int
 import Data.IORef
-import GHC.Word
 
 initUI :: IO Gtk.Application
 initUI = do
-  ioRef <- newIORef undefined
   app <- new Gtk.Application [#applicationId := "hexournal"]
-  on app #activate (activate app ioRef)
-  id #run app Nothing
+  _ <- on app #activate (do
+      appWin <- new Gtk.ApplicationWindow [#application := app, #startupId := "appWin id", #title := "appWin title"]
+      drawingArea <- new Gtk.DrawingArea [#hasTooltip := True, #name := "drawingArea"]
+      set appWin [#child := drawingArea]
+      -- slightly irked out by that overload
+      --id #setDrawFunc drawingArea (Just (drawCb ioRef))
+      Gtk.drawingAreaSetDrawFunc drawingArea (Just undefined)
+      _ <- after drawingArea #resize resizeCb
+      gestureStylus <- new Gtk.GestureStylus [#name := "gestureStylus"]
+      Gtk.widgetAddController drawingArea gestureStylus
+      _ <- on gestureStylus #down $ stylusDownCb undefined
+      _ <- on gestureStylus #proximity $ stylusProximityCb undefined
+      _ <- on gestureStylus #motion $ stylusMotionCb undefined
+      _ <- on gestureStylus #up $ stylusUpCb undefined
+      id #show appWin
+      )
+  _ <- #run app Nothing
   return app
 
-activate :: Gtk.Application -> IORef GI.Cairo.Context -> IO ()
-activate app ioRef = do
-  win <- new Gtk.ApplicationWindow [#application := app, #title := "title"]
-  dA <- new Gtk.DrawingArea []
-  set win [#child := dA]
-  return False
-  id #setDrawFunc dA (Just (drawCb ioRef))
-  after dA #resize resizeCb
-  gS <- new Gtk.GestureStylus []
-  Gtk.widgetAddController dA gS
-  on gS #down $ stylusDownCb ioRef
-  on gS #proximity $ stylusProximityCb ioRef
-  on gS #motion $ stylusMotionCb ioRef
-  on gS #up $ stylusUpCb ioRef
-  id #show win
-  return ()
-
-drawCb :: IORef GI.Cairo.Context -> Gtk.DrawingArea -> GI.Cairo.Context -> Int32 -> Int32 -> IO ()
+drawCb :: IORef GI.Cairo.Context -> (Gtk.DrawingArea -> GI.Cairo.Context -> Int32 -> Int32 -> IO ())
 drawCb ioRef drawingArea cr width height = do
 {-
   renderWithContext (Cairo.setSourceRGB (fromIntegral 0) (fromIntegral 0) 1)  cr
