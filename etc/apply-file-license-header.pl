86050d9415e35bd4dacef65beb07a2cdd8134be3
Johnson He <hejohns@umich.edu>
2021-05-23 01:44:32 +0000

    idk why I keep wasting my time w/ this perl script
    turns out closed type families can be used to "constrain type classes to
    a finite list of types"
    something something singleton kinds maybe...

--- etc/apply-file-license-header.pl
+++ etc/apply-file-license-header.pl
@@ -13,15 +13,21 @@ use File::Spec;
 use Cwd qw(abs_path);
 
 sub print_help{
-my $zero = basename $0;
-my $dir = dirname $0;
-my $help = <<~"EOF"
-Usage: ./$zero
-Description:
-    must be called in $dir
-EOF
-;
-print $help;
+    my $zero = basename $0;
+    my $dir = dirname $0;
+    my $help = <<~"EOF"
+    Usage: ./$zero
+    Description:
+        this help message will print on all errrors
+        ./$zero must be called with cwd = $dir
+        (cause File::Find is broken...)
+
+        header block must be contigously predicated by
+        (is_comment | is_preserved_comment)
+        see defintions below
+    EOF
+    ;
+    print $help;
 }
 
 ##############################
@@ -31,9 +37,11 @@ my $file_license_header = 'file-license-header.txt';
 # avoid File::Spec->rel2abs
 my $project_dir = abs_path(File::Spec->updir($0));
 # map file extentions to \@header -> @header
-my %map_header_lines = ( hs => sub{$_ = shift; map {'-- ' . $_} @{$_}}
+my %map2comment = ( '.hs' => sub{$_ = shift; map {'-- ' . $_} @{$_}}
 );
-my %delete_contiguous_match = ( hs => sub{$_ = shift; $_ =~ m/^--/}
+my %is_comment = ( '.hs' => sub{shift =~ m/^--/}
+);
+my %is_preserved_comment = ( '.hs' => sub{shift =~ m/^-- \*/}
 );
 ##############################
 
@@ -41,25 +49,36 @@ my @flh;
 
 # see File::Find
 sub wanted{
+    # [warning] File::Find and File::Spec are very broken
     my ($filename, $dir, $file_extension) = fileparse($File::Find::name, qw(.hs));
-    $file_extension = substr $file_extension, 1;
     unless(defined $file_extension and $file_extension ne ''){
         return 1;
     }
     # cause File::Find is so broken
-    open(my $FH, '<:encoding(UTF-8)', $filename . ".$file_extension");
+    open(my $FH, '<:encoding(UTF-8)', $filename . $file_extension);
     my @file = <$FH>;
-    while(defined $file[0] and $delete_contiguous_match{$file_extension}($file[0])){
-        shift @file;
+    my @preserved_comments;
+    while(defined $file[0]){
+        if($is_preserved_comment{$file_extension}($file[0])){
+            push(@preserved_comments, shift @file);
+        }
+        elsif($is_comment{$file_extension}($file[0])){
+            shift @file;
+        }
+        else{
+            last;
+        }
     }
     # cause File::Find is so broken
     (my $file_src_path = $File::Find::name) =~ s/.*\/src\/(.*)/$1/;
     say $file_src_path;
-    my @header = ($file_src_path . "\n", "\n", @flh);
-    @header = $map_header_lines{$file_extension}(\@header);
+    my @file_src_path = ($file_src_path . "\n", "\n");
+    my @header = ($map2comment{$file_extension}(\@file_src_path),
+        @preserved_comments,
+        $map2comment{$file_extension}(\@flh));
     my @file = (@header, @file);
     close($FH);
-    open($FH, '>:encoding(UTF-8)', $filename . ".$file_extension");
+    open($FH, '>:encoding(UTF-8)', $filename . $file_extension);
     print $FH @file;
     close($FH);
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 -- Main.hs
 -- 
+-- * this is a preserved comment (see apply-file-license-header.pl)
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation; either version 2 of the License, or
